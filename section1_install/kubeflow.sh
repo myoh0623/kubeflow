1cef61f9c10c8e6648f3270587d0488601aecfab
Donutman <110797599+myoh0623@users.noreply.github.com>
2022-08-18 19:49:20 +0900

    Update kubeflow.sh

--- section1_install/kubeflow.sh
+++ section1_install/kubeflow.sh
@@ -1,3 +1,3 @@
 kubectl delete deployments.apps training-operator -n kubeflow
 kustomize build ~/kubeflow/manifests/apps/training-operator/upstream/overlays/kubeflow | kubectl apply -f -
-while true; do kubectl port-forward --address="0.0.0.0" svc/istio-ingressgateway -n istio-system 8080:80; done
+while true; do kubectl port-forward --address="0.0.0.0" svc/istio-ingressgateway -n istio-system 8080:443; done
