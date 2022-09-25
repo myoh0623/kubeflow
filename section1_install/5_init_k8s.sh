0b35e37bfe3d654797dcbff94b2f9662e537f79a
Donutman <110797599+myoh0623@users.noreply.github.com>
2022-09-25 09:18:19 +0900

    Update 5_init_k8s.sh
    change to flannel
    kustomize version 3.10.0

--- section1_install/5_init_k8s.sh
+++ section1_install/5_init_k8s.sh
@@ -9,8 +9,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl cluster-info
 
-kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.6/install/kubernetes/quick-install.yaml
-kubectl get pods -n kube-system --selector=k8s-app=cilium
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/v0.13.0/Documentation/kube-flannel.yml
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta6/nvidia-device-plugin.yml
@@ -27,8 +26,8 @@ kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storagec
 kubectl get sc
 
 # install kusomize 
-wget https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
-mv kustomize_3.2.0_linux_amd64 kustomize
+wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.10.0/kustomize_v3.10.0_linux_amd64.tar.gz
+tar -zxvf kustomize_v3.10.0_linux_amd64.tar.gz
 sudo chmod 777 kustomize
 sudo mv kustomize /usr/local/bin
 
