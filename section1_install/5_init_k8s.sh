c3236900db3ae9306b1327d40dd297d9e389ba51
Donutman <110797599+myoh0623@users.noreply.github.com>
2022-09-26 11:30:06 +0900

    Update 5_init_k8s.sh

--- section1_install/5_init_k8s.sh
+++ section1_install/5_init_k8s.sh
@@ -10,12 +10,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl cluster-info
 
 # CNI
-# kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.9.9/install/kubernetes/quick-install.yaml
-# kubectl get pods -n kube-system --selector=k8s-app=cilium
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml 
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
-
+kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.6/install/kubernetes/quick-install.yaml
+kubectl get pods -n kube-system --selector=k8s-app=cilium
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/1.0.0-beta6/nvidia-device-plugin.yml
