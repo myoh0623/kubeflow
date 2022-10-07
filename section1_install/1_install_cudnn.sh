0aa45f1f8df2cc12c8bc38b1e74a0f3100951134
한양남자 <110797599+myoh0623@users.noreply.github.com>
2022-10-07 23:05:35 +0900

    Update 1_install_cudnn.sh

--- section1_install/1_install_cudnn.sh
+++ section1_install/1_install_cudnn.sh
@@ -8,10 +8,10 @@ nvcc -V
 whereis cuda
 mkdir ~/nvidia
 cd ~/nvidia
-wget http://people.cs.uchicago.edu/~kauffman/nvidia/cudnn/cudnn-10.1-linux-x64-v7.6.5.32.tgz
-tar -xvfz cudnn-10.1-linux-x64-v7.6.5.32.tgz
-tar -xvzf cudnn-10.1-linux-x64-v7.6.5.32.tgz
-ls
+CUDNN_TAR_FILE="cudnn-10.1-linux-x64-v7.6.5.32.tgz"
+wget https://developer.download.nvidia.com/compute/redist/cudnn/v7.6.5/${CUDNN_TAR_FILE}
+tar -xzvf ${CUDNN_TAR_FILE}
+
 sudo cp cuda/include/cudnn.h /usr/lib/cuda/include/
 sudo cp cuda/lib64/libcudnn* /usr/lib/cuda/lib64/
 sudo chmod a+r /usr/lib/cuda/lib64/libcudnn*
