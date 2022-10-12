2f0b39d8d78aa0821d81d9379bcc8d419adceba6
한양남자 <110797599+myoh0623@users.noreply.github.com>
2022-10-12 11:25:16 +0900

    Update 5_init_k8s.sh

--- section1_install/5_init_k8s.sh
+++ section1_install/5_init_k8s.sh
@@ -29,7 +29,7 @@ kubectl get sc
 
 # install kusomize 
 # 
-if [ -f /usr/local/bin/kusomize ]
+if [ ! -f /usr/local/bin/kusomize ]
   then
     echo "kustomize"
     wget https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
