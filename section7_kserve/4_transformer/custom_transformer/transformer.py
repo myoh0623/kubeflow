ff38ad9218508da789964e680c7545eb9ca6b45a
myoh0623@gmail.com <myoh0623@gmail.com>
2022-10-15 12:16:00 +0900

    transformer

--- section7_kserve/4_transformer/custom_transformer/transformer.py
+++ section7_kserve/4_transformer/custom_transformer/transformer.py
@@ -4,6 +4,9 @@ import argparse
 import urllib
 import numpy as np
 import PIL.Image as Image
+from urllib import request
+from io import BytesIO
+
 
 class ImageTransformer(kserve.Model):
     def __init__(self, name: str, predictor_host: str):
@@ -13,7 +16,6 @@ class ImageTransformer(kserve.Model):
         imagenet_labels = np.array(open(labels_path).read().splitlines())
         self.imagenet_labels = imagenet_labels
         self.IMAGE_SHAPE = (224, 224)
-
         self.ready = True
         
     def image_transform(self, instance) -> List:
@@ -21,18 +23,19 @@ class ImageTransformer(kserve.Model):
         res = request.urlopen(image_url).read()
         image_data_rs = Image.open(BytesIO(res)).resize(self.IMAGE_SHAPE)
         image_data_rs_n = np.array(image_data_rs)/255.0
-        X_data = image_data_rs_n[np.newaxis, ...].tolist()
+        X_data = image_data_rs_n.tolist()
         return X_data
         
     def preprocess(self, inputs: Dict) -> Dict:
         instance = {'instances': [self.image_transform(instance) for instance in inputs['instances']]}
-        return {'instances': instance}
+        return instance
 
     def postprocess(self, model_output_data: Dict) -> Dict:
-        pred = model_output_data.json()['predictions'][0]
-        ppredicted_class = np.argmax(pred, axis=-1)
+        pred = model_output_data['predictions'][0]
+        predicted_class = np.argmax(pred, axis=-1)
         predicted_class_name = self.imagenet_labels[predicted_class]
         return {"predictions" : predicted_class_name}
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(parents=[kserve.model_server.parser])
